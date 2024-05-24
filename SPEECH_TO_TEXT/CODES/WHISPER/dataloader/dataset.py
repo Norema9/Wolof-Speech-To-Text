3c8367fc5c0d78f7f0bc262e8af3eb87bad7be06
Norema9 <97368427+Norema9@users.noreply.github.com>
2024-05-24 23:25:14 +0200

    finetune whisper

---
+++ SPEECH_TO_TEXT/CODES/WHISPER/dataloader/dataset.py
@@ -0,0 +1,58 @@
+import torch
+from typing import Any, Dict, List, Union
+from utils.feature import load_wav
+from typing import Dict
+from transformers import WhisperProcessor
+
+class DefaultCollate:
+    """ DataCollatorSpeechSeq2SeqWithPadding """
+    def __init__(self, processor:WhisperProcessor) -> None:
+        self.processor = processor
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lengths and need different padding methods
+        # first treat the audio inputs by simply returning torch tensors
+        input_features = [{"input_features": feature["input_features"]} for feature in features]
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+
+        # get the tokenized label sequences
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        # pad the labels to max length
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
+
+        # replace padding with -100 to ignore loss correctly
+        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+
+        # if bos token is appended in previous tokenization step,
+        # cut bos token here as it's append later anyways
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
+
+        batch["labels"] = labels
+
+        return batch
+
+class Dataset:
+    def __init__(self, data, sr, preload_data, feature_extractor, transform = None):
+        self.data = data
+        self.sr = sr
+        self.transform = transform
+        self.preload_data = preload_data
+        self.feature_extractor = feature_extractor
+        
+    def __len__(self) -> int:
+        return len(self.data)
+        
+    def __getitem__(self, idx) -> tuple:
+        item = self.data.iloc[idx]
+        if not self.preload_data:
+            audio = load_wav(item['path'], sr = self.sr)
+        else:
+            audio = item['wav']
+
+        feature = self.feature_extractor(audio, sampling_rate = self.sr).input_features[0]
+
+        features = {"input_features": feature, "labels": item["transcript"]}
+        
+        return features
+
---
+++ SPEECH_TO_TEXT/CODES/WHISPER/utils/metric.py
@@ -0,0 +1,19 @@
+from datasets import load_metric
+import torch
+
+class Metric:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.wer_metric = load_metric("wer")
+    def __call__(self, logits, label_ids):
+        pred_ids = torch.argmax(logits, axis=-1)
+        label_ids[label_ids == -100] = self.processor.tokenizer.pad_token_id
+
+        # we do not want to group tokens when computing the metrics
+        pred_strs = self.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        label_strs = self.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+
+        wer = self.wer_metric.compute(predictions=pred_strs, references=label_strs)
+        return wer
+    
+    
